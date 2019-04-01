9b4cbf10d98add58dd94c7f16b74e6ced5ae1c4a
Влада <vladaoboenkova@gmail.com>
2019-04-01 08:07:29 +0300

    Исправление замечаний, добавление тестов

--- src/test/java/task/TrainTest.java
+++ src/test/java/task/TrainTest.java
@@ -22,6 +22,19 @@ public class TrainTest {
 
     @Test
     public void removeStation() {
+        Train train1 = new Train("Sapsan123", "Moscow");
 
+        train1.addStation("A");
+        train1.addStation("B");
+
+        train1.removeStation("A");
+
+        List<String> actual = new ArrayList<>();
+        actual.add("B");
+
+        Assert.assertEquals(train1.getListOfStations(), actual);
+
+        train1.removeStation("A");
+        Assert.assertEquals(train1.getListOfStations(), actual);
     }
 }
